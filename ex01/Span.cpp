ad704ad758c88706be03fd3878b561c94835e7f5
Nerea Gastanaga Artetxe <ngastana@c4r1s5.42urduliz.com>
2025-08-16 19:53:09 +0200

    starting ex01

---
+++ ex01/Span.cpp
@@ -0,0 +1,50 @@
+#include "Span.hpp"
+
+Span::Span(unsigned int N): _N(N){
+    return;
+}
+
+Span::~Span(){
+}
+
+Span::Span(const Span &other){
+    *this = other;
+    return;
+}
+
+Span& Span::operator=(const Span& other) {
+    this->_N = other._N;
+    return (*this);
+}
+
+void Span::addNumber(int n) {
+    if (_numbers.size() >= _N)
+        throw NoSpaceForMoreElements();
+    _numbers.push_back(n);
+}
+
+unsigned int Span::longestSpan() {
+    if (_numbers.size() < 2)
+        throw LessThanOneNumberStores();
+
+    int maxVal = *std::max_element(_numbers.begin(), _numbers.end());
+    int minVal = *std::min_element(_numbers.begin(), _numbers.end());
+    return maxVal - minVal;
+}
+
+unsigned int Span::shortestSpan() {
+    if (_numbers.size() < 2)
+        throw LessThanOneNumberStores();
+
+    std::vector<int> sorted(_numbers);
+    std::sort(sorted.begin(), sorted.end()); //de menor a mayor orden ascendente
+
+    int minSpan = 4294967295;
+    //int minSpan = std::numeric_limits<int>::max();
+    for (size_t i = 0; i < sorted.size() - 1; ++i) {
+        int diff = sorted[i+1] - sorted[i];
+        if (diff < minSpan)
+            minSpan = diff;
+    }
+    return minSpan;
+}
---
+++ ex01/Span.hpp
@@ -0,0 +1,55 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <iterator>
+#include <limits>
+
+class Span{
+    private:
+        Span();
+        unsigned int _N;
+        std::vector<int> _numbers;
+
+    public:
+        Span(unsigned int N);
+        Span(const Span &copy);
+        Span &operator=(const Span &other);
+        ~Span();
+        void addNumber(int n);
+        unsigned int shortestSpan();
+        unsigned int longestSpan();
+        
+        //Last but not least, it would be wonderful to fill your Span using a range of iterators. Making thousands of calls to addNumber() is so annoying. Implement a member function to add multiple numbers to your Span in a single call.
+        template <typename it>
+        void addRange(it first, it last) {
+            if (_numbers.size() + std::distance(first, last) > _N)
+                throw NoSpaceForMoreElements();
+            _numbers.insert(_numbers.end(), first, last);
+        }
+
+    class NoSpaceForMoreElements: public std::exception
+    {
+        public:
+            virtual const char *what() const throw()
+            {
+                return("No more space for the new element.");
+            }
+    };
+
+    class LessThanOneNumberStores: public std::exception
+    {
+        public:
+            virtual const char *what() const throw()
+            {
+                return("There are no numbers stored, or only one, no span can be found.");
+            }
+    };
+};
+
+
+
+#endif
